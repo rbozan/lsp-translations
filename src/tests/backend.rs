a5443e795d4efd718273f32c4b70e53ef2f2cde6
Robin Bozan <robinbozan@gmail.com>
2021-09-17 15:24:40 +0200

    WIP: Adds completion test

--- src/tests/backend.rs
+++ src/tests/backend.rs
@@ -1,14 +1,12 @@
-
-
 #[cfg(test)]
 mod tests {
-    
-    use tower_lsp::{MessageStream};
+
+    use tower_lsp::MessageStream;
     use tower_test::mock::Spawn;
 
     use super::*;
+    use tower_lsp::jsonrpc::Response;
     use tower_lsp::jsonrpc::{Incoming, Outgoing};
-    use tower_lsp::jsonrpc::{Response};
     use tower_lsp::{Client, LanguageServer, LspService, Server};
 
     use core::task::Poll;
@@ -20,15 +18,27 @@ mod tests {
     use futures::select;
     use futures::{FutureExt, StreamExt};
 
-    // use core::stream::Stream;
-
-    const INITIALIZED_REQUEST: &str = r#"{"jsonrpc":"2.0","method":"initialized","params":{}}"#;
-
     lazy_static! {
         static ref INITIALIZE_REQUEST: Incoming = serde_json::from_str(
             r#"{"jsonrpc":"2.0","method":"initialize","params":{"capabilities":{}},"id":1}"#
         )
         .unwrap();
+
+
+        static ref INITIALIZE_RESPONSE: Outgoing = Outgoing::Response(serde_json::from_str(r#"{
+            "jsonrpc":"2.0",
+            "result": {
+                "capabilities": {
+                    "completionProvider": {"resolveProvider": true},
+                    "hoverProvider": true,
+                    "textDocumentSync": 1,
+                    "workspace": {"workspaceFolders": {"changeNotifications": true, "supported": true}}
+                }
+            },
+            "id":1
+        }"#).unwrap());
+
+        static ref INITIALIZED_REQUEST: Incoming = serde_json::from_str(r#"{"jsonrpc":"2.0","method":"initialized","params":{}}"#).unwrap();
 
         static ref WORKSPACE_CONFIGURATION_REQUEST: Incoming = serde_json::from_str(
             r#"{"jsonrpc":"2.0","result": [
@@ -76,20 +86,42 @@ mod tests {
             .as_str()
         )
         .unwrap();
-    }
 
-    const INITIALIZE_RESPONSE: &str = r#"{
+        static ref COMPLETION_REQUEST: Incoming = serde_json::from_str(r#"{
             "jsonrpc":"2.0",
-            "result": {
-                "capabilities": {
-                    "completionProvider": {"resolveProvider": true},
-                    "hoverProvider": true,
-                    "textDocumentSync": 1,
-                    "workspace": {"workspaceFolders": {"changeNotifications": true, "supported": true}}
+            "method":"textDocument/completion",
+            "params":{
+                "textDocument": {
+                    "uri": "file:///home/rbozan/Projects/recharge-mobile-app/backend/src/route/v1/translation.js"
+                },
+                "position": {
+                    "line": 5,
+                    "character": 0
+                },
+                "context": {
+                    "triggerKind": 1
                 }
             },
             "id":1
-        }"#;
+        }"#).unwrap();
+
+        static ref COMPLETION_RESPONSE: Outgoing =  Outgoing::Response(serde_json::from_str(r#"{
+            "jsonrpc":"2.0",
+                            "id": 1,
+            "result": [
+                {
+                    "detail": "definition:  Definition {\n    key: \"1234567890.en-us.main.content.heading.body\",\n    cleaned_key: Some(\n        \"main.content.heading.body\",\n    ),\n    file: \"\",\n    language: None,\n    value: \"This is the body of my website.\"}",
+                    "kind": 1,
+                    "label": "main.content.heading.body"
+                },
+                {
+                    "detail": "definition: Definition {\n    key: \"1234567890.en-us.main.content.heading.title\",\n    cleaned_key: Some(\n        \"main.content.heading.title\",\n    ),\n    file: \"\",\n    language: None,\n    value: \"A regular header for my content\"}",
+                    "kind": 1,
+                    "label": "main.content.heading.body"
+                }
+            ]
+        }"#).unwrap());
+    }
 
     fn init_service() -> (Spawn<LspService>, MessageStream) {
         let (service, messages) = LspService::new(|client| Backend::new(client));
@@ -101,9 +133,11 @@ mod tests {
     async fn initialize() {
         let (mut service, _) = init_service();
 
-        let ok = Outgoing::Response(serde_json::from_str::<Response>(INITIALIZE_RESPONSE).unwrap());
         assert_eq!(service.poll_ready(), Poll::Ready(Ok(())));
-        assert_eq!(service.call(INITIALIZE_REQUEST.clone()).await, Ok(Some(ok)));
+        assert_eq!(
+            service.call(INITIALIZE_REQUEST.clone()).await,
+            Ok(Some(INITIALIZE_RESPONSE.clone()))
+        );
 
         let raw = r#"{"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid request"},"id":1}"#;
         let err = Outgoing::Response(serde_json::from_str::<Response>(raw).unwrap());
@@ -115,30 +149,30 @@ mod tests {
     }
 
     #[tokio::test]
-    #[timeout(6000)]
+    #[timeout(8000)]
     async fn send_configuration() {
         let (mut service, mut messages) = init_service();
 
-        let ok = Outgoing::Response(serde_json::from_str::<Response>(INITIALIZE_RESPONSE).unwrap());
-        assert_eq!(service.call(INITIALIZE_REQUEST.clone()).await, Ok(Some(ok)));
-
-        let shutdown: Incoming = serde_json::from_str(INITIALIZED_REQUEST).unwrap();
+        assert_eq!(
+            service.call(INITIALIZE_REQUEST.clone()).await,
+            Ok(Some(INITIALIZE_RESPONSE.clone()))
+        );
 
         assert_eq!(service.poll_ready(), Poll::Ready(Ok(())));
 
         select!(
-            req = service.call(shutdown.clone()).fuse() => {
+            req = service.call(INITIALIZED_REQUEST.clone()).fuse() => {
                 assert_eq!(req.unwrap(), None);
             },
             () = handle_lsp_message(
-                service,
+                &mut service,
                 &mut messages,
                 vec![
                     &*WORKSPACE_CONFIGURATION_REQUEST,
                     &*WORKSPACE_WORKSPACE_FOLDERS_REQUEST,
                 ],
             ).fuse() => {
-                panic!("lsp messages should not finish faster than finishing request")
+                panic!("lsp messages should not finish faster than request")
             },
         );
 
@@ -149,41 +183,37 @@ mod tests {
     }
 
     #[tokio::test]
-    #[timeout(6000)]
+    #[timeout(8000)]
     async fn completion() {
-        let (mut service, mut messages) = init_service();
+        let (mut service, mut messages) = prepare_workspace().await;
+        // TODO: Completion request
+        //
+        assert_eq!(
+            service.call(COMPLETION_REQUEST.clone()).await,
+            Ok(Some(COMPLETION_RESPONSE.clone()))
+        );
 
-        let ok = Outgoing::Response(serde_json::from_str::<Response>(INITIALIZE_RESPONSE).unwrap());
-        assert_eq!(service.call(INITIALIZE_REQUEST.clone()).await, Ok(Some(ok)));
-
-        let shutdown: Incoming = serde_json::from_str(INITIALIZED_REQUEST).unwrap();
-
-        assert_eq!(service.poll_ready(), Poll::Ready(Ok(())));
-
+        /*
         select!(
-            req = service.call(shutdown.clone()).fuse() => {
-                assert_eq!(req.unwrap(), None);
+            req = service.call(COMPLETION_REQUEST.clone()).fuse() => {
+                assert_eq!(req.unwrap(), Ok(Some(COMPLETION_RESPONSE.clone())));
             },
             () = handle_lsp_message(
-                service,
+                &mut service,
                 &mut messages,
                 vec![
                     &*WORKSPACE_CONFIGURATION_REQUEST,
                     &*WORKSPACE_WORKSPACE_FOLDERS_REQUEST,
                 ],
             ).fuse() => {
-                panic!("lsp messages should not finish faster than finishing request")
+                panic!("lsp messages should not finish faster than request")
             },
-        );
-
-        let message = (messages.next().await).unwrap();
-        let value = serde_json::to_value(message).unwrap();
-
-        assert_eq!(value["params"]["message"], "Loaded 3 definitions");
+        ); */
     }
 
+    // Helper function
     async fn handle_lsp_message(
-        mut service: Spawn<LspService>,
+        service: &mut Spawn<LspService>,
         messages: &mut MessageStream,
         responses: Vec<&Incoming>,
     ) {
@@ -210,6 +240,37 @@ mod tests {
 
             // if i >= responses.len() { break; }
         }
-        ()
+    }
+
+    async fn prepare_workspace() -> (Spawn<LspService>, MessageStream) {
+        let (mut service, mut messages) = init_service();
+
+        assert_eq!(
+            service.call(INITIALIZE_REQUEST.clone()).await,
+            Ok(Some(INITIALIZE_RESPONSE.clone()))
+        );
+
+        assert_eq!(service.poll_ready(), Poll::Ready(Ok(())));
+
+        select!(
+            req = service.call(INITIALIZED_REQUEST.clone()).fuse() => {
+                assert_eq!(req.unwrap(), None);
+            },
+            () = handle_lsp_message(
+                &mut service,
+                &mut messages,
+                vec![
+                    &*WORKSPACE_CONFIGURATION_REQUEST,
+                    &*WORKSPACE_WORKSPACE_FOLDERS_REQUEST,
+                ],
+            ).fuse() => {
+                panic!("lsp messages should not finish faster than finishing request")
+            },
+        );
+
+        let message = (messages.next().await).unwrap();
+        let value = serde_json::to_value(message).unwrap();
+
+        (Spawn::new(service.into_inner()), messages)
     }
 }
