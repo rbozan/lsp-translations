cb98f94fadc6f7731d38e5540c83d49a038d20b0
Robin Bozan <robinbozan@gmail.com>
2021-10-27 15:44:16 +0200

    feat: Adds definition language by file name

--- src/tests/definition.rs
+++ src/tests/definition.rs
@@ -5,9 +5,9 @@ fn converts_en_to_us_flag() {
     let definition = Definition {
         key: "en.test_key".to_string(),
         cleaned_key: Some("test_key".to_string()),
-        file: "somefile.json".to_string(),
         language: Some("en".to_string()),
         value: "some value".to_string(),
+        ..Default::default()
     };
 
     assert_eq!(definition.get_flag(), Some("🇺🇸".to_string()));
@@ -18,9 +18,9 @@ fn printable_value_escapes_newlines() {
     let definition = Definition {
         key: "en.test_key".to_string(),
         cleaned_key: Some("test_key".to_string()),
-        file: "somefile.json".to_string(),
         language: Some("en".to_string()),
         value: "\nSome value with multiple\nnewlines".to_string(),
+        ..Default::default()
     };
 
     assert_eq!(
@@ -34,9 +34,9 @@ fn printable_value_escapes_vertical_line() {
     let definition = Definition {
         key: "en.test_key".to_string(),
         cleaned_key: Some("test_key".to_string()),
-        file: "somefile.json".to_string(),
         language: Some("en".to_string()),
         value: "Abc|defg".to_string(),
+        ..Default::default()
     };
 
     assert_eq!(definition.get_printable_value(), "Abc\\|defg");
@@ -46,10 +46,8 @@ fn printable_value_escapes_vertical_line() {
 fn printable_value_doesnt_escape_arabic() {
     let definition = Definition {
         key: "test_key".to_string(),
-        cleaned_key: None,
-        file: "somefile.json".to_string(),
-        language: None,
         value: "مهلا".to_string(),
+        ..Default::default()
     };
 
     assert_eq!(definition.get_printable_value(), "مهلا");
